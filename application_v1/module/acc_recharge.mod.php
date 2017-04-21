2ae6002c6666d55f142d0bc1e455e7bcdc785871
lehw <lehw@1.com>
2017-04-21 17:44:39 +0800

    image_change

--- application_v1/module/acc_recharge.mod.php
+++ application_v1/module/acc_recharge.mod.php
@@ -32,8 +32,8 @@ if(!empty($userInfo)){
     <div class="close"><img src="<?php echo CDN_SERVER;?>images/recharge_Manage/icon-close.png" alt=""></div>
     <h2>微信扫一扫支付马上完成</h2>
     <p >交易金额 : <span class="account"> 100.00元</span></p>
-    <p>交易号 : <span class="acc_num"> XZ201704191458401798</span></p>
-    <img src="<?php echo CDN_SERVER;?>images/common/wePay.png" alt="">
+    <p>交易号 : <span class="acc_num" id="wx_billNo"> XZ201704191458401798</span></p>
+    <img src="<?php echo CDN_SERVER;?>images/common/wePay.png" id="wx_img" alt="" width="300px" height="300px">
     <h3>请使用微信扫描二维码以完成支付</h3>
 </div>
 <!--未选择账户提示框-->
@@ -56,6 +56,12 @@ if(!empty($userInfo)){
             </p>
             <div class="full_gl"></div>
             <!--账号充值部分-->
+             <form method="post" action="<?php echo API_URL;?>pay.php" target="_blank" class="payForm" id="zfb_form" enctype="application/x-www-form-urlencoded">
+                <input type="hidden" id="payType" name="payType" value="3"/>
+                <input type="hidden" id="price" name="price" value="10"/>
+                <input type="hidden" id="userPId" name="userPId" value="0"/>
+            </form>
+
             <div class="sys_mess one">
                 <ul class="mess_ul">
                     <li class="active_x">
@@ -133,7 +139,7 @@ if(!empty($userInfo)){
                         <p class="bl">充值成功后，您将获得 <b class="coin_num"> 1000</b> <em class="coin_txt">搜虎币</em> </p>
                     <div class="pay_money" >
                         <h4>支付金额 : <em>10</em> 元 </h4>
-                        <button type="submit" class="surePay weiPay">确认支付</button>
+                        <button type="button" class="surePay weiPay">确认支付</button>
                     </div>
                     </form>
                 </div>
@@ -141,7 +147,7 @@ if(!empty($userInfo)){
             <!--支付宝支付-->
             <div class="mess_detail" style="display: none">
                 <div class="mess">
-                    <form method="post" action="<?php echo API_URL;?>pay.php" target="_blank" class="payForm">
+                     <form action="javascript:void(0)" class="payForm">
                     <p class="fh_tit">
                         <span>支付宝支付</span>
                     </p>
@@ -150,7 +156,7 @@ if(!empty($userInfo)){
                     <ul class="person_ul">
                         <li>
                             <span class="pro_span">官网账号:<?php echo $userInfo->nickName;?></span>
-                            <img class="che_img" src="<?php echo CDN_SERVER;?>images/recharge_Manage/has_che.png" alt="" value="1">
+                            <img class="che_img" src="<?php echo CDN_SERVER;?>images/recharge_Manage/has_che.png" alt="" value="0">
                         </li>
                         <?php
                             if(!empty($userProdurceList)){
@@ -158,7 +164,7 @@ if(!empty($userInfo)){
                         ?>
                              <li class="mg_lt">
                                 <span class="pro_span">飞虎账号:<?php echo $row->pnickName;?></span>
-                                <img class="che_img" src="<?php echo CDN_SERVER;?>images/recharge_Manage/has_che.png" hidden>
+                                <img class="che_img" src="<?php echo CDN_SERVER;?>images/recharge_Manage/has_che.png" value="<?php echo $row->id;?>" hidden>
                             </li>
                         <?
                                 }
@@ -191,7 +197,7 @@ if(!empty($userInfo)){
                         <p class="bl">充值成功后，您将获得 <b class="coin_num"> 1000</b> <em class="coin_txt">搜虎币</em> </p>
                         <div class="pay_money">
                             <h4>支付金额 : <em>10</em> 元 </h4>
-                            <button type="submit" class="surePay ">确认支付</button>
+                            <button type="button" class="surePay"  id="zfb_button">确认支付</button>
                        </div>
                     </form>
                 </div>
@@ -278,6 +284,17 @@ $(function(){
         }
         var index_z=$(this).index();
         $(".mess_detail").eq(index_z).css("display","block").siblings().css("display","none");
+        if(index_z == 0){
+           $('#payType').val(3);
+           $('#price').val(10);
+        }else if(index_z == 1){
+           $('#payType').val(2);
+           $('#price').val(10);
+        }else{
+           $('#payType').val(1);
+           $('#price').val(1000);
+        }
+
     });
     $(".know").click(function () {
         $(".selAcc_pop").css("display",'none');
@@ -285,6 +302,38 @@ $(function(){
     });
     //点击微信支付
     $('.weiPay').click(function () {
+
+        var payType = parseInt($('#payType').val());
+        var userPId = $('#userPId').val();
+        var price = parseInt($('#price').val());
+        if(payType < 1 ){
+            $('.selAcc_pop h3 strong').html('请选择支付方式!');
+            $(".selAcc_pop").css("display",'block');
+            $(".opacity_color").css("display",'block');
+            return;
+        }
+        var data = {payType:payType,userPId:userPId,price:price};
+        $.post('<?php echo API_URL;?>pay.php',data,function(result){
+            if(result == null){
+                //生成订单失败
+                return;
+            }
+            var jsonData = JSON.parse(result);
+            var code = jsonData.code;
+           
+            if(code == "0"){
+                $(".account").html(mr_money+'元');
+                $('#wx_img').attr('src',jsonData.url);
+                $('#wx_billNo').html(jsonData.billNo);
+                $(".wePay_pop").css("display","block");
+                $(".opacity_color").css("display",'block');
+            }
+            else{
+                //
+               
+            }
+        });
+        /*
         $(".account").html(mr_money+'元');
         //确认用户是否选择账户
         var is_vi=$('.person_ul li img').is(":visible");
@@ -293,19 +342,50 @@ $(function(){
             $(".selAcc_pop").css("display",'block');
             $(".opacity_color").css("display",'block');
         }else{
+            
             $(".wePay_pop").css("display","block");
             $(".opacity_color").css("display",'block');
-        }
+        }*/
     });
     //支付宝支付
-    $('.payForm').submit(function () {
+    // $('.payForm').submit(function () {
 
+    // });
+    $('#zfb_button').click(function(){
+        var payType = parseInt($('#payType').val());
+        var userPId = $('#userPId').val();
+        var price = parseInt($('#price').val());
+        if(payType < 1 ){
+            $('.selAcc_pop h3 strong').html('请选择支付方式!');
+            $(".selAcc_pop").css("display",'block');
+            $(".opacity_color").css("display",'block');
+            return;
+        }
+        if(price < 1 ){
+            $('.selAcc_pop h3 strong').html('请输入大于0的整数金额!');
+            $(".selAcc_pop").css("display",'block');
+            $(".opacity_color").css("display",'block');
+            return;
+        }
+        $('#zfb_form').submit(); 
+        // var is_vi=$('.person_ul li img').is(":visible");
+        // if(!is_vi){
+        //     $('.selAcc_pop h3 strong').html('请选择所要充值的账号!');
+        //     $(".selAcc_pop").css("display",'block');
+        //     $(".opacity_color").css("display",'block');
+        //     return;
+        // }
+        // $userPId = $('.person_ul li img').val();
+        // alert($userPId);
+      // $('.payForm').form(); 
     });
     //点击弹出框关闭按钮
     $(".close").click(function(){
         $(this).parent().css("display","none");
         $(".opacity_color").css("display",'none');
     });
+
+
     //点击选择不同账户
     $(".person_ul li").click(function () {
         var index_c=$(this).index();
@@ -318,11 +398,14 @@ $(function(){
         var if_see=$(this).children('img').is(':visible');
         if(!if_see){
             $(this).children('img').css("display","inline");
+            $('#userPId').val($(this).children('img'));
             if( $(this).siblings().children('img').is(':visible')){
                 $(this).siblings().children('img').css("display","none");
             }
+
         }else{
             $(this).children('img').css("display","none");
+            $('#userPId').val($(this).children('img').val());
         }
     });
     //点击不同充值金额 金额支付
@@ -334,6 +417,7 @@ $(function(){
         $(".user_sel").removeClass("active_sel");
         var rmb=$(this).children(".money").attr("value");
         $(".pay_money em").html(rmb);
+        $('#price').val(rmb);
         mr_money= $(".pay_money em").html();
         $(".bl .coin_num").html(rmb*100);
     });
